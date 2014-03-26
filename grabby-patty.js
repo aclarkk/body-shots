942e513db275fdf9f17e4ebd517f609e1083b3ab
Alan Clark <hello@aclarkcreative.com>
2014-03-26 18:21:50 -0400

    Loop through sizes instead of hard coding

--- grabby-patty.js
+++ grabby-patty.js
@@ -2,48 +2,45 @@
 var url ='http://dribbble.com';
 
 //path to local save folder
-var savePath='exports/';
+var savePath='screengrabs/';
 
-//use date in save string
 var date = new Date();
 
-//initial setup
-var casper = require('casper').create({    
-    viewportSize: {width: 1680, height: 1500},
-    verbose: true,
-    logLevel: "info"
+var delay = 100;
+
+var selector
+
+viewportSizes=[
+  [1650, 1080],
+  [720, 1024],
+  [320, 700]
+]
+
+var casper = require('casper').create();
+
+casper.start();
+casper.each(viewportSizes, function(self, viewportSize, i){
+  var width = viewportSize[0],
+      height = viewportSize[1];
+
+  casper.wait(delay, function() {
+
+    this.viewport(width, height);
+
+    casper.thenOpen(url, function() {
+      this.viewport(width, height);
+      this.captureSelector(savePath + 'GP' + width + height + '.png','body');
+
+      this.echo('screenshot taken at ' + width + 'x' + height);
+    });
+
+  });
 });
 
-//desktop capture
-casper.start(url, function(){
-	this.captureSelector( savePath + 'screengrab' + date +'.png', 'body');
-});
-
-//tablet capture
-casper.then(function(){
-	casper.log("grabbing the tablet", "info");
-
-	casper.viewport(768, 1024);
-	casper.userAgent('Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) version/4.0.4 Mobile/7B367 Safari/531.21.10');
-
-    this.captureSelector( savePath + 'screengrab' + date +'-tablet.png', 'body');
-
-});
-
-//mobile capture
-casper.then(function(){
-	casper.log("grabbing the mobile", "info");
-
-	casper.viewport(320, 1500);
-	casper.userAgent('Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A522a Safari/419.3');
-
-    this.captureSelector( savePath + 'screengrab' + date +'-mobile.png', 'body');
-
-});
 
 //run & done
 casper.run(function() {
-    this.echo('\n\n\n*********** Done ***********\n\n\n').exit();
+  this.echo('\n\n\n*********** Done ***********\n\n\n').exit();
 });
 
 
